f749f0feeca21be1f8345e4130ddb5f3f5ca7bda
vanessalaffont <vanessa.laffont@gmail.com>
2021-06-21 17:56:36 +0200

    lazy-loading-avec-angular partie 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,20 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './modules/general/home/home.component';
-import { ContactComponent } from './modules/general/contact/contact.component';
 import { NotFoundComponent } from './modules/general/not-found/not-found.component';
-import { AboutComponent } from './modules/general/about/about.component';
-import { SigninComponent } from './modules/general/signin/signin.component';
-import { TestComponent } from './modules/general/test/test.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ContactComponent,
-    NotFoundComponent,
-    AboutComponent,
-    SigninComponent,
-    TestComponent
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
